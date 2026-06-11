4372d87404f08f181280c1dc3d5f9d1587748ade
Abhishek <hello@upbringapp.com>
2026-06-12 00:04:45 +0530

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -58,6 +59,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-ZJEPH34628"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-ZJEPH34628');
+  `}
+</Script>
         {children}
       </body>
     </html>
