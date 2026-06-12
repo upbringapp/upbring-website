510b48451dea0828c64d8b45547cc6a22a8c5f30
Abhishek <hello@upbringapp.com>
2026-06-12 11:03:00 +0530

    Add matadataBase

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://upbringapp.com"),
   title: "Upbring | Raising Curious Minds",
   description:
     "Helping families nurture curiosity, character and a lifelong love of learning.",
