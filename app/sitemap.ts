0999559d1252e4d82947785480652c69040081fa
Abhishek <hello@upbringapp.com>
2026-08-06 16:35:42 +0530

    Withdraw incomplete blog from publication

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["/", "/about", "/blog", "/contact", "/privacy", "/terms"];
+  const routes = ["/", "/about", "/contact", "/privacy", "/terms"];
 
   return routes.map((route) => ({
     url: new URL(route, "https://upbringapp.com").toString(),
